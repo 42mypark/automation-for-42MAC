4e14866e6f5c44b807e68763b4969edf647eca5c
Myeonghyeon Park <mypark@c1r3s6.42seoul.kr>
2022-06-02 10:08:47 +0900

    mkdir_vscode_cache.sh

---
+++ mkdir_vscode_cache.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+USER=mypark
+GOINFRE=/Users/$USER/goinfre
+CACHE=$GOINFRE/Cache
+
+
+
+mkdir -pv $CACHE/.vscode/extensions
+mkdir -pv $CACHE/Library/Application\ Support/Code/User/workspaceStorage
+mkdir -pv $CACHE/Library/Application\ Support/Code/CachedData
+mkdir -pv $CACHE/Library/Application\ Support/Code/CachedExtensionVSIXs
+mkdir -pv $CACHE/Library/Application\ Support/Code/CachedExtensions
+mkdir -pv $CACHE/Library/Application\ Support/Code/Code\ Cache
+
+
+
+
